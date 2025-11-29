7ef3a52eccaffbbe76253856f679d760f0e2c1fa
Paameth <diallohameth130@gmail.com>
2025-11-29 22:04:28 +0000

    addition

---
+++ demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "demo.h"
+
+void say_hello() {
+    printf("Hello from demo.c !\n");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ demo.h
@@ -0,0 +1,8 @@
+#ifndef DEMO_H
+#define DEMO_H
+
+void say_hello();
+int add(int a, int b);
+
+
+#endif
